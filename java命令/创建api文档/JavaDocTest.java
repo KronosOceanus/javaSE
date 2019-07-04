23188e98b5473830061f41a95ef3a6063088e0cc
KronosOceanus <704690152@qq.com>
2019-07-04 11:25:06 +0800

    更新

---
+++ java命令/创建api文档/JavaDocTest.java
@@ -0,0 +1,13 @@
+package cs;
+public class JavaDocTest
+{
+	/**
+	*一个得到打招呼字符串的方法
+	*@param name 该参数指定向谁打招呼
+	*@return 返回打招呼的字符串
+	*/
+	public string hello(String name)
+	{
+		return name+",hello!";
+	}
+}
