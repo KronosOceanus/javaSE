5133ca4c45728c45a86157d0b36ba04ff7f206e4
KronosOceanus <44320710+KronosOceanus@users.noreply.github.com>
2020-11-12 10:35:54 +0800

    Update Main.java

--- 简单工厂模式/src/cs/Main.java
+++ 简单工厂模式/src/cs/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args)
     {
-        //创建一个电脑带有OutPut接口
+        //创建一个带有Output接口（即Printer实例）的电脑
         Computer c = new Computer(OutputFactory.getOutput());
         c.keyIn("java");
         c.keyIn("i love you!");
